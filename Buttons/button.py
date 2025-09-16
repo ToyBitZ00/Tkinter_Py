08e63c595bef73ebcd6112299001870982eba7ec
ToyBitZ00 <pa6308191@gmail.com>
2025-09-17 00:38:55 +0800

    Added simple button interaction.

---
+++ Buttons/button.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+# button = you click it, then it does stuff
+
+window = Tk()
+
+button = Button(window,
+                text="Click me!")
+
+button.pack()
+window.mainloop()
