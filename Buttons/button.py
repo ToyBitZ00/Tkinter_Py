1dfb44e1a07f2f316d5acb7cbd2167de748680bd
ToyBitZ00 <pa6308191@gmail.com>
2025-09-17 00:40:51 +0800

    Made a click function, when clicked it will print a text.

--- Buttons/button.py
+++ Buttons/button.py
@@ -2,10 +2,16 @@ from tkinter import *
 
 # button = you click it, then it does stuff
 
+
+def click():
+    print("You clicked the button")
+
+
 window = Tk()
 
 button = Button(window,
-                text="Click me!")
+                text="Click me!",
+                command=click)
 
 button.pack()
 window.mainloop()
