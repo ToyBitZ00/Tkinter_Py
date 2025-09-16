a61232eb7846db183842c664cadf7207f2692b5e
ToyBitZ00 <pa6308191@gmail.com>
2025-09-17 00:46:07 +0800

    Changed the active foreground and background to remove the changing colors when the button is clicked.

--- Buttons/button.py
+++ Buttons/button.py
@@ -14,7 +14,9 @@ button = Button(window,
                 command=click,
                 font=("Comic Sans", 30),
                 fg="#00FF00",
-                bg="black")
+                bg="black",
+                activeforeground="#00FF00",
+                activebackground="black")
 
 button.pack()
 window.mainloop()
