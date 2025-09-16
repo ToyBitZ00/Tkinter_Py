4b403f47808734cae666cb4a90cc34b49b3d7ee9
ToyBitZ00 <pa6308191@gmail.com>
2025-09-17 00:17:54 +0800

    In Line 11, Added Font name, size, and type for the text.

--- window.py
+++ window.py
@@ -1,14 +1,14 @@
 from tkinter import *
 
 window = Tk()  # Used to instantiate an instance of a windows
-window.geometry("420x420")
+window.geometry("600x600")
 window.title("My First GUI Program")
 icon = PhotoImage(file='App Icon.png')
 window.iconphoto(True, icon)
 window.config(background="cyan")  # You can use RGB or Hex Code
 
 # label = an area widget that holds text and/or an image within a window
-label = Label(window, text="Hello World")
+label = Label(window, text="Hello World", font=('Arial', 40, 'bold'))
 label.pack()
 # label.place(x=100, y=100)
 
