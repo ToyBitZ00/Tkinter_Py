0ca7428de8103c99043ad34df6f8d398456bed64
ToyBitZ00 <pa6308191@gmail.com>
2025-09-17 00:12:41 +0800

    Added a simple label widget to display a string.

--- window.py
+++ window.py
@@ -3,10 +3,12 @@ from tkinter import *
 window = Tk()  # Used to instantiate an instance of a windows
 window.geometry("420x420")
 window.title("My First GUI Program")
-
 icon = PhotoImage(file='App Icon.png')
 window.iconphoto(True, icon)
-
 window.config(background="black")  # You can use RGB or Hex Code
+
+# label = an area widget that holds text and/or an image within a window
+label = Label(window, text="Hello World")
+label.pack()
 
 window.mainloop()  # Place window on the screen.
