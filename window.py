be756c9a91420f71bb649de75c4c23b3b284d26d
ToyBitZ00 <pa6308191@gmail.com>
2025-09-17 00:10:11 +0800

    Changed the background color using config, added some comments for readability.

--- window.py
+++ window.py
@@ -1,10 +1,12 @@
 from tkinter import *
 
-window = Tk()
+window = Tk()  # Used to instantiate an instance of a windows
 window.geometry("420x420")
 window.title("My First GUI Program")
 
 icon = PhotoImage(file='App Icon.png')
 window.iconphoto(True, icon)
 
-window.mainloop()
+window.config(background="black")  # You can use RGB or Hex Code
+
+window.mainloop()  # Place window on the screen.
