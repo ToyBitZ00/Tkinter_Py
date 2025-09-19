755ab1c00492f038ee63df73f65af30527a295fb
ToyBitZ00 <pa6308191@gmail.com>
2025-09-19 13:27:44 +0800

    Created submit button, and then configured it, to display the user input from entry button

--- userInput.py
+++ userInput.py
@@ -1,9 +1,26 @@
 from tkinter import *
 
+
+def submit():
+    print("Hello", entry.get())
+
+
 window = Tk()
+window.title("Entry Box Program")
 
 entry = Entry(window,
-              font=("Arial", 50))
+              font=("Arial", 50),
+              bg="black",
+              fg="#00FF00")
 
-entry.pack()
+entry.pack(side=LEFT)
+
+submit_button = Button(window,
+                       text="Submit",
+                       command=submit,
+                       font=('Comis Sans', 20),
+                       compound="right")
+
+submit_button.pack(side=RIGHT)
+
 window.mainloop()
