946e1e70fed3dd62a38082097b2ac7673bbabb24
ToyBitZ00 <pa6308191@gmail.com>
2025-09-19 13:38:20 +0800

    Added Delete and Backspace button to modify the user input from entry button, also created function for the button to work.

--- userInput.py
+++ userInput.py
@@ -2,25 +2,34 @@ from tkinter import *
 
 
 def submit():
-    print("Hello", entry.get())
+    username = entry.get()
+    print("Hello", username)
+
+
+def delete():
+    entry.delete(0, END)
+
+
+def backspace():
+    entry.delete(len(entry.get())-1, END)
 
 
 window = Tk()
 window.title("Entry Box Program")
 
-entry = Entry(window,
-              font=("Arial", 50),
-              bg="black",
-              fg="#00FF00")
-
+entry = Entry(window, font=("Arial", 50), bg="black", fg="#00FF00")
 entry.pack(side=LEFT)
 
-submit_button = Button(window,
-                       text="Submit",
-                       command=submit,
-                       font=('Comis Sans', 20),
-                       compound="right")
-
+submit_button = Button(window, text="Submit",
+                       command=submit, font=('Comis sans', 20))
 submit_button.pack(side=RIGHT)
+
+delete_button = Button(window, text="Delete",
+                       command=delete, font=('Comic sans', 20))
+delete_button.pack(side=RIGHT)
+
+backspace_button = Button(window, text="Backspace",
+                          command=backspace, font=('Comic sans', 20))
+backspace_button.pack(side=RIGHT)
 
 window.mainloop()
