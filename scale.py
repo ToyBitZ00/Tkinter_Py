483a7b1bb8496d6b2ba27bb6dbc607464d2d4c49
ToyBitZ00 <pa6308191@gmail.com>
2025-09-22 17:32:12 +0800

    Defined the scale, and button for submit function.

--- scale.py
+++ scale.py
@@ -1,8 +1,19 @@
 from tkinter import *
 
+
+def submit():
+    print("The temperature is:", scale.get(), "degrees C")
+
+
 window = Tk()
 
-scale = Scale(window, from_=0, to=100)
+scale = Scale(window,
+              from_=0,
+              to=100)
 scale.pack()
 
+button = Button(window,
+                text=('Submit'),
+                command=submit)
+button.pack()
 window.mainloop()
