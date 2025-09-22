8e0eb4ab6f67939cc3347a94b4f11bd891bdc491
ToyBitZ00 <pa6308191@gmail.com>
2025-09-22 17:07:56 +0800

    Changed the anchor of the text to West, added x padding, and a font for readbility.

--- radiobuttons.py
+++ radiobuttons.py
@@ -7,9 +7,14 @@ window = Tk()
 x = IntVar()
 
 for index in range(len(food)):
-    radioButton = Radiobutton(
-        window, text=food[index], variable=x, value=index)
-    radioButton.pack()
+    radioButton = Radiobutton(window,
+                              text=food[index],
+                              variable=x,
+                              value=index,
+                              padx=25,
+                              font=('Impact', 50)
+                              )
+    radioButton.pack(anchor=W)
 
 
 window.mainloop()
