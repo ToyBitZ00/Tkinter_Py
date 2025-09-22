dd9d421b870d88d9162f3ba6429055fabcd7cd4d
ToyBitZ00 <pa6308191@gmail.com>
2025-09-22 17:18:59 +0800

    Uploaded three food images, converted it and then used it, so the choices wll match the index and display the appropriate image for the choices.

--- radiobuttons.py
+++ radiobuttons.py
@@ -4,6 +4,11 @@ food = ["Pizza", "Hamburger", "Hotdog"]
 
 window = Tk()
 
+pizzaImage = PhotoImage(file='pizza.png')
+hamburgerImage = PhotoImage(file='hamburger.png')
+hotdogImage = PhotoImage(file='hotdog.png')
+
+foodImages = [pizzaImage, hamburgerImage, hotdogImage]
 x = IntVar()
 
 for index in range(len(food)):
@@ -12,7 +17,9 @@ for index in range(len(food)):
                               variable=x,
                               value=index,
                               padx=25,
-                              font=('Impact', 50)
+                              font=('Impact', 50),
+                              image=foodImages[index],
+                              compound='left'
                               )
     radioButton.pack(anchor=W)
 
