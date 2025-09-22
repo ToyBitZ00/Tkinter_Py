49fcd750e98abbd5e07d1542b729cee1081a9f5a
ToyBitZ00 <pa6308191@gmail.com>
2025-09-22 17:39:48 +0800

    added some config options like tickInterval, showValue and troughColor.

--- scale.py
+++ scale.py
@@ -2,18 +2,28 @@ from tkinter import *
 
 
 def submit():
-    print("The temperature is:", scale.get(), "degrees C")
+    print("The temperature is:" + str(scale.get()) + " degrees C")
 
 
 window = Tk()
 
 scale = Scale(window,
-              from_=0,
-              to=100)
+              from_=100,
+              to=0,
+              length=600,
+              orient=VERTICAL,  # VERTICAL or HORIZONTAL
+              font=('Consolas', 20),
+              tickinterval=10,  # Adds numeric indicators for value
+              showvalue=0,  # No longer shows the value when scrolling
+              troughcolor='#69EAFF'
+
+              )
 scale.pack()
 
 button = Button(window,
                 text=('Submit'),
-                command=submit)
+                command=submit
+                )
 button.pack()
+
 window.mainloop()
