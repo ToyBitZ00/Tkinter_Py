fd7496be286ea9418c56483c4c390e80eb5d7e72
ToyBitZ00 <pa6308191@gmail.com>
2025-09-22 17:03:36 +0800

    Added variable for integer value that is the index of the chosen food.

--- radiobuttons.py
+++ radiobuttons.py
@@ -3,7 +3,13 @@ from tkinter import *
 food = ["Pizza", "Hamburger", "Hotdog"]
 
 window = Tk()
+
+x = IntVar()
+
 for index in range(len(food)):
-    radioButton = Radiobutton(window, text=food[index])
+    radioButton = Radiobutton(
+        window, text=food[index], variable=x, value=index)
     radioButton.pack()
+
+
 window.mainloop()
